63b4ff4d4343fb70017f157c976cbf4a3bd72a4a
Weverton Ribeiro Ferreira <82607987+wevertonribeiroferreira@users.noreply.github.com>
2022-04-02 15:22:19 -0500

    Update helloworld_start.py

--- Ch2 - Basics/helloworld_start.py
+++ Ch2 - Basics/helloworld_start.py
@@ -1,6 +1,14 @@
 #
 # Example file for HelloWorld
-# LinkedIn Learning Python course by Joe Marini
+# Created a function called main where
 #
 
+def main(): 
+    print("Hello World!")
+    name = input("What is your name?")
+    print("Nice to meet you", name)
 
+#execute as a main program (own programa)
+
+if __name__=="__main__":
+     main()
